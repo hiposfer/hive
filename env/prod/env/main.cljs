7e6d3be230be8507dea71fc34a137db003657b5b
carocad <carocad@unal.edu.co>
2017-11-18 18:45:49 +0100

    init commit

---
+++ env/prod/env/main.cljs
@@ -0,0 +1,4 @@
+(ns env.main
+  (:require [hive.core :as core]))
+
+(core/init)
---
+++ src/hive/core.cljs
@@ -0,0 +1,33 @@
+(ns hive.core
+    (:require [reagent.core :as r :refer [atom]]
+              [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+              [hive.handlers]
+              [hive.subs]))
+
+(def ReactNative (js/require "react-native"))
+
+(def app-registry (.-AppRegistry ReactNative))
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def view (r/adapt-react-class (.-View ReactNative)))
+(def image (r/adapt-react-class (.-Image ReactNative)))
+(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+(def Alert (.-Alert ReactNative))
+
+(defn alert [title]
+  (.alert Alert title))
+
+(defn app-root []
+  (let [greeting (subscribe [:get-greeting])]
+    (fn []
+      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+       [image {:source (js/require "./assets/images/cljs.png")
+               :style {:width 200
+                       :height 200}}]
+       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
+       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                             :on-press #(alert "HELLO!")}
+        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+
+(defn init []
+  (dispatch-sync [:initialize-db])
+  (.registerComponent app-registry "main" #(r/reactify-component app-root)))
