c366ac5bebc073fb4116886dac0bcd6209113fd9
carocad <carocad@unal.edu.co>
2017-11-20 17:44:37 +0100

    adapt cities.json to datascript

--- src/hive/state.cljs
+++ src/hive/state.cljs
@@ -6,8 +6,15 @@
 
 (def schema {:user/city {:db.valueType     :db.type/ref
                          :db.cardinality   :db.cardinality/one}
-             :route/stack {:db.valueType   :db.type/string
-                           :db.cardinality :db.cardinality/many}})
+             :user/id {:db.unique :db.unique/identity}
+             ;:route/stack {:db.valueType   :db.type/string
+             ;              :db.cardinality :db.cardinality/many}
+             :city/name {:db.unique :db.unique/identity}})
 
 ;; needs to be an array of maps. This will be used for data/transact!
-(def defaults cities)
+(def defaults
+  (concat (sequence (comp (map #(assoc % :city/name (:name %)))
+                         (map #(dissoc % :name)))
+                    cities)
+          [{:user/id -1 ;; dummy
+            :user/city [:city/name "Frankfurt am Main"]}]))
