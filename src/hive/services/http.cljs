39739fc5652f941bf0ac42b1b1b16c49b76faa48
Camilo Roca <camilo.roca@cobi.bike>
2018-01-09 15:52:46 -0500

    fix: removed unnecessary functions

--- src/hive/services/http.cljs
+++ src/hive/services/http.cljs
@@ -8,41 +8,34 @@
 (s/def ::url (s/and string? not-empty))
 (s/def ::request (s/cat :URL ::url :options (s/? ::init)))
 
-(defn- request!
-  "takes an http channel and a request shaped according to
-  spec's ::request and executes it asynchronously. Extra http
-  properties can be passed as per fetch documentation.
-  Returns a channel with the result of fetch, an exception on error
-  or a Promise if ::http/raw is chosen. Throws on invalid request
+(defn json!
+  "takes a request shaped according to ::request and executes it asynchronously.
+   Extra http properties can be passed as per fetch documentation.
+  Returns a channel with the result of fetch as a JS object or an exception on error
 
   https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch"
   [[url init]]
-  (js/fetch url (clj->js init)))
-
-(defn- json
-  [promise]
   (let [result (async/chan)]
-    (-> promise
+    (-> (js/fetch url (clj->js init))
         (.then #(.json %))
         (.then #(async/put! result %))
         (.catch #(async/put! result (ex-info "network error" %
                                              ::network-error))))
     result))
 
-(defn- text
-  [promise]
+(defn text!
+  "takes a request shaped according to ::request and executes it asynchronously.
+   Extra http properties can be passed as per fetch documentation.
+  Returns a channel with the result of fetch as a String or an exception on error
+
+  https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch"
+  [[url init]]
   (let [result (async/chan)]
-    (-> promise
+    (-> (js/fetch url (clj->js init))
         (.then #(.text %))
         (.then #(async/put! result %))
         (.catch #(async/put! result (ex-info "network error" %
                                              ::network-error))))
     result))
-
-(def json! (rework/pipe request!
-                        json))
-
-(def text! (rework/pipe request!
-                        text))
 
 (s/fdef request! :args (s/cat :request ::request))
