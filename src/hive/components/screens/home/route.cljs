2f6a1fa90d3918c069f0fb2c125539f7bb788fe3
carocad <carocad@unal.edu.co>
2018-02-09 12:08:45 +0100

    fix: fetch next route on "next". Cache previous routes on database fix: remove other routes whenever the user selects a route fix: remove all routes whenever the user selects a target; just in case

--- src/hive/components/screens/home/route.cljs
+++ src/hive/components/screens/home/route.cljs
@@ -23,19 +23,9 @@
              prepare-coordinates
              directions/request!))
 
-;(defn- tx-path
-;  "takes a mapbox directions response and returns a transaction vector
-;  to use with transact!. Return an exception if no path was found"
-;  [path]
-;  (let [id      (:uuid path)
-;        garbage (remove #{id} (:route/remove path))]
-;    (concat (map #(vector :db.fn/retractEntity [:route/uuid %]) garbage)
-;            [(tool/with-ns :route (dissoc path :user/id))
-;             {:user/id (:user/id path)
-;              :user/directions [:route/uuid id]}])))
-
 (defn- validate-path
-  "takes a mapbox directions response and returns it.
+  "takes a mapbox directions response and returns it with :route
+   as namespaced keys instead of its own.
    Return an exception if no path was found"
   [path]
   (cond
@@ -54,58 +44,86 @@
   (work/pipe get-path!
              validate-path))
 
+(defn choose-path
+  "assocs the path to :user/directions and remove all other temporary paths"
+  [path]
+  (let [id      (:route/uuid path)
+        garbage (remove #{id} (:route/remove path))]
+    (concat (map #(vector :db.fn/retractEntity [:route/uuid %]) garbage)
+            [{:user/id         (:user/id path)
+              :user/directions [:route/uuid id]}])))
+
+(def set-route!
+  "takes a mapbox directions object and assocs the user/directions with
+  it. All other temporary paths are removed"
+  (work/pipe (work/inject :user/id queries/user-id)
+             (work/inject :route/remove queries/routes-ids)
+             choose-path
+             work/transact!))
+
+(defn- next-path!
+  "get the next route in memory or fetch one otherwise"
+  [routes i goal]
+  (swap! i inc)
+  (when (nil? (get @routes (inc @i)))
+    (go-try (work/transact! (<? (fetch-path! @goal))))))
+
 (defn route-details
-  [props routes i]
-  (println)
-  (let [directions   (work/entity [:route/uuid (get @routes @i)])
-        route        (first (:route/routes directions))
+  [props i]
+  (let [routes       (work/q! queries/routes-ids)
+        path         (work/entity [:route/uuid (get @routes @i)])
+        goal         (work/q! queries/user-goal)
+        route        (first (:route/routes path))
         instructions (sequence (comp (mapcat :steps)
                                      (map :maneuver)
                                      (map :instruction)
                                      (map-indexed vector))
                                (:legs route))]
-    [:> base/Card
-     [:> base/CardItem [:> base/Icon {:name "flag"}]
-      [:> base/Text (str "distance: " (:distance route) " meters")]]
-     [:> base/CardItem [:> base/Icon {:name "flag"}]
-      [:> base/Text (str "UUID: " (:route/uuid directions) " meters")]]
-     [:> base/CardItem [:> base/Icon {:name "information-circle"}]
-      [:> base/Text "duration: " (Math/round (/ (:duration route) 60)) " minutes"]]
-     [:> base/CardItem [:> base/Icon {:name "time"}]
-      [:> base/Text (str "time of arrival: " (js/Date. (+ (js/Date.now)
-                                                          (* 1000 (:duration route))))
-                         " minutes")]]
-     [:> react/View {:style {:flexDirection "row" :alignItems "flex-start"
-                             :flex 1}}
-      [:> base/Button {:danger true :bordered false
-                       :on-press #(swap! i dec)}
-       [:> base/Icon {:name "ios-arrow-back"}]
-       [:> base/Text "previous"]]
-      [:> base/Button {:success true :iconRight true :bordered false
-                       :on-press #(swap! i inc)}
-       [:> base/Text "next"]
-       [:> base/Icon {:name "ios-arrow-forward"}]]]
-     [:> base/CardItem [:> base/Icon {:name "map"}]
-      [:> base/Text "Instructions: "]]
-     (for [[id text] instructions]
-       ^{:key id}
-       [:> base/CardItem
-        (if (= id (first (last instructions)))
-          [:> base/Icon {:name "flag"}]
-          [:> base/Icon {:name "ios-navigate-outline"}])
-        [:> base/Text text]])]))
+    (if (nil? path)
+      [:> base/Spinner]
+      [:> base/Card
+       [:> base/CardItem [:> base/Icon {:name "flag"}]
+        [:> base/Text (str "distance: " (:distance route) " meters")]]
+       [:> base/CardItem [:> base/Icon {:name "flag"}]
+        [:> base/Text (str "UUID: " (:route/uuid path) " meters")]]
+       [:> base/CardItem [:> base/Icon {:name "information-circle"}]
+        [:> base/Text "duration: " (Math/round (/ (:duration route) 60)) " minutes"]]
+       [:> base/CardItem [:> base/Icon {:name "time"}]
+        [:> base/Text (str "time of arrival: " (js/Date. (+ (js/Date.now)
+                                                            (* 1000 (:duration route))))
+                           " minutes")]]
+       [:> react/View {:style {:flexDirection "row" :justifyContent "space-around"
+                               :flex 1}}
+        (when (> @i 0)
+          [:> base/Button {:warning true :bordered false
+                           :on-press #(swap! i dec)}
+            [:> base/Icon {:name "ios-arrow-back"}]
+            [:> base/Text "previous"]])
+        [:> base/Button {:success true :bordered false
+                         :on-press #(do (set-route! (into {} path))
+                                        ((:goBack (:navigation props))))}
+          [:> base/Text "OK"]]
+        [:> base/Button {:warning true :iconRight true :bordered false
+                         :on-press #(next-path! routes i goal)}
+          [:> base/Text "next"]
+          [:> base/Icon {:name "ios-arrow-forward"}]]]
+       [:> base/CardItem [:> base/Icon {:name "map"}]
+        [:> base/Text "Instructions: "]]
+       (for [[id text] instructions]
+         ^{:key id}
+         [:> base/CardItem
+          (if (= id (first (last instructions)))
+            [:> base/Icon {:name "flag"}]
+            [:> base/Icon {:name "ios-navigate-outline"}])
+          [:> base/Text text]])])))
 
 (defn instructions
   "basic navigation directions"
   [props]
-  (let [routes  (work/q! queries/routes-ids)
-        goal    (work/q! queries/user-goal)
-        counter (r/atom 0)]
-    (when (nil? (get @routes (inc @counter)))
-      (go-try (work/transact! (<? (fetch-path! @goal)))))
+  (let [counter (r/atom 0)]
     (fn []
       [:> base/Container
        [:> base/Content
-        [route-details props routes counter]]])))
+        [route-details props counter]]])))
 
 ;(work/q queries/routes-ids)
