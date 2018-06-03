2b7fbbd46f58b1265e2a629170d9f7dbb32e7320
Camilo Roca <camilo.roca@cobi.bike>
2018-06-03 17:39:42 +0200

    refactored selector to use only react native not native base

--- src/hive/components/screens/settings/city_picker.cljs
+++ src/hive/components/screens/settings/city_picker.cljs
@@ -1,7 +1,6 @@
 (ns hive.components.screens.settings.city-picker
   (:require [hive.rework.core :as work]
             [hive.services.store :as store]
-            [hive.components.foreigns.native-base :as base]
             [hive.queries :as queries]
             [hive.components.symbols :as symbols]
             [hive.components.foreigns.react :as react]
@@ -15,7 +14,7 @@
     (store/save! (select-keys tx [:user/city]))
     (goBack)))
 
-(defn city-selector
+(defn city-entry
   [props]
   (let [params   (:params (:state (:navigation props)))
         city     (:city props)
@@ -37,17 +36,14 @@
   [props]
   (let [params   (:params (:state (:navigation props)))
         cities  @(work/q! queries/cities)
-        id       (:user/id params)
-        goBack   (:goBack (:navigation props))]
-    [:> base/Container
-     [:> base/Header
-      [:> base/Left
-       [:> base/Button {:onPress #(goBack)}
-        [:> base/Icon {:name "arrow-back"}]]]
-      [:> base/Body
-       [:> base/Title "Select City"]]]
-     [:> react/View {:style {:flex 1 :backgroundColor "white"}}
-      (for [city cities]
-        ^{:key (:city/name city)}
-        [city-selector (assoc props :city city :user/id id)])]]))
+        id       (:user/id params)]
+    [:> react/View {:style {:flex 1}}
+      [:> react/View {:style {:height 60 :alignItems "center" :justifyContent "center"
+                              :backgroundColor "blue"}}
+        [:> react/Text {:style {:color "white" :fontSize 20}}
+          "Select City"]]
+      [:> react/View {:style {:flex 9 :backgroundColor "white"}}
+        (for [city cities]
+          ^{:key (:city/name city)}
+          [city-entry (assoc props :city city :user/id id)])]]))
 
