ac913028e992a7e162cc75cb77e5f99d4cb98754
Camilo Roca <camilo.roca@cobi.bike>
2018-07-13 14:28:21 +0200

    cosmetic changes

--- src/hive/components/screens/errors.cljs
+++ src/hive/components/screens/errors.cljs
@@ -7,10 +7,9 @@
 
 (defn- launch-location-settings
   "launch the android location settings hoping that the user enables the gps"
-  [props]
+  [goBack]
   (if (= "android" (.. fl/ReactNative -Platform.OS))
-    (let [settings (.. fl/Expo -IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS)
-          goBack   (:goBack (:navigation props) settings)]
+    (let [settings (.. fl/Expo -IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS)]
       [(delay (.. fl/Expo (IntentLauncherAndroid.startActivityAsync settings)))
        [goBack]])))
 
@@ -18,27 +17,27 @@
   [props]
   (let [dims   (tool/keywordize (.. fl/ReactNative (Dimensions.get "window")))
         goBack (:goBack (:navigation props))]
-    [:> react/View {:style {:flex 1 :backgroundColor "white" :paddingVertical "20%"
-                            :elevation 5 :shadowColor "#000000"
-                            :shadowRadius 5 :shadowOffset {:width 0 :height 3}
-                            :shadowOpacity 1.0}}
+    [:> react/View {:flex 1 :backgroundColor "white" :paddingVertical "20%"
+                    :elevation 5 :shadowColor "#000000"
+                    :shadowRadius 5 :shadowOffset {:width 0 :height 3}
+                    :shadowOpacity 1.0}
      [:> react/Image {:style  {:width (* (:width dims) 0.8)
                                :height (* (:height dims) 0.5)
                                :resizeMode "contain" :flex 1}
                       :source fl/thumb-run}]
-     [:> react/View {:style {:height 200 :alignItems "center"}}
+     [:> react/View {:height 200 :alignItems "center"}
       [:> react/Text {:style {:flexWrap "wrap"}}
         "We couldn't find your current location"]
       [:> react/Text]
       [:> react/Text "Please enable your GPS to continue"]
-      [:> react/View {:style {:flexDirection "row" :alignItems "flex-start" :flex 1}}
+      [:> react/View {:flexDirection "row" :alignItems "flex-start" :flex 1}
        [:> react/TouchableOpacity
          {:style {:borderRadius 5 :backgroundColor "red" :height 40 :width 60
                   :justifyContent "center" :alignItems "center"}
           :on-press #(goBack)}
          [:> expo/Ionicons {:name "ios-close-circle" :size 30}]]
        [:> react/TouchableOpacity
-         {:on-press #(run! work/transact! (launch-location-settings props))
+         {:on-press #(run! work/transact! (launch-location-settings goBack))
           :style {:borderRadius 5 :backgroundColor "lawngreen"
                   :height 40 :width 60 :flexDirection "row"
                   :alignItems "center" :justifyContent "space-around"}}
