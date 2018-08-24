1ddc37ce1ae76e30fbb40dddea0d3fbb6556117b
Camilo Roca <camilo.roca@cobi.bike>
2018-08-24 12:05:06 +0200

    cosmetic changes

--- src/hive/components/screens/settings/core.cljs
+++ src/hive/components/screens/settings/core.cljs
@@ -28,33 +28,33 @@
         [:> react/Text {:style {:color "white" :fontSize 20}}
                        "Settings"]]
      ;; USER INFO ---------------
-     [:> react/View {:height 125 :paddingBottom 20 :paddingTop 20}
-       [:> react/Text {:style {:color "slategray" :fontSize 15
-                               :paddingLeft 20 :justifyContent "flex-end"}}
-                      (str/upper-case "user info")]
-       [:> react/View {:height 50 :flex-direction "row"}
-         [:> react/View {:width 50 :height 50 :margin 5 :alignItems "center"
-                         :justifyContent "center" :backgroundColor "lightgray"
-                         :borderRadius 50/2}
-           [:> expo/Ionicons {:name "ios-person-outline" :size 30}]]
-         [:> react/View {:flex 0.7 :justifyContent "flex-end"}
-           [:> react/Text "USERNAME"]
-           [:> react/Text {:note true :style {:color "gray"}}
-                          "email"]]]]
+      [:> react/View {:height 125 :paddingBottom 20 :paddingTop 20}
+        [:> react/Text {:style {:color "slategray" :fontSize 15
+                                :paddingLeft 20 :justifyContent "flex-end"}}
+                       (str/upper-case "user info")]
+        [:> react/View {:height 50 :flex-direction "row"}
+          [:> react/View {:width 50 :height 50 :margin 5 :alignItems "center"
+                          :justifyContent "center" :backgroundColor "lightgray"
+                          :borderRadius 50/2}
+            [:> expo/Ionicons {:name "ios-person-outline" :size 30}]]
+          [:> react/View {:flex 0.7 :justifyContent "flex-end"}
+            [:> react/Text "USERNAME"]
+            [:> react/Text {:note true :style {:color "gray"}}
+                           "email"]]]]
       ;; USER CITY ---------------
-     [:> react/View {:height 125}
-      [:> react/Text {:style {:color "slategray" :fontSize 15 :paddingLeft 20
-                              :justifyContent "flex-end"}}
-                     (str/upper-case "current city")]
-      [:> react/TouchableOpacity {:onPress #(navigate "select-city"
-                                                      {:user/uid  id
-                                                       :city/name (:city/name (:user/city city))})
-                                  :style {:height 45}}
-        [symbols/PointOfInterest
-          [:> expo/Ionicons {:name "md-map" :size 26}]
-          [:> react/Text ""]
-          [:> react/Text (:city/name (:user/city city))]
-          [:> react/Text {:style {:color "gray"}}
-                         (str (:city/region (:user/city city)) ", "
-                              (:city/country (:user/city city)))]
-          [:> expo/Ionicons {:name "ios-checkmark" :size 26}]]]]]))
+      [:> react/View {:height 125}
+       [:> react/Text {:style {:color "slategray" :fontSize 15 :paddingLeft 20
+                               :justifyContent "flex-end"}}
+                      (str/upper-case "current city")]
+       [:> react/TouchableOpacity {:onPress #(navigate "select-city"
+                                                       {:user/uid  id
+                                                        :city/name (:city/name (:user/city city))})
+                                   :style {:height 45}}
+         [symbols/PointOfInterest
+           [:> expo/Ionicons {:name "md-map" :size 26}]
+           [:> react/Text ""]
+           [:> react/Text (:city/name (:user/city city))]
+           [:> react/Text {:style {:color "gray"}}
+                          (str (:city/region (:user/city city)) ", "
+                               (:city/country (:user/city city)))]
+           [:> expo/Ionicons {:name "ios-checkmark" :size 26}]]]]]))
