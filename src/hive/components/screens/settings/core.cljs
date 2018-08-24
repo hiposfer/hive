ead94b1cdacc17ba51f88058a9ca67de3c8df0f8
Camilo Roca <camilo.roca@cobi.bike>
2018-08-24 12:14:24 +0200

    cosmetic changes

--- src/hive/components/screens/settings/core.cljs
+++ src/hive/components/screens/settings/core.cljs
@@ -23,11 +23,12 @@
         navigate (:navigate (:navigation props))
         visible? (r/atom false)]
     [:> react/View {:flex 1}
+    ;; HEADER -----------------
       [:> react/View {:height 60 :alignItems "center" :justifyContent "center"
                       :backgroundColor "blue"}
         [:> react/Text {:style {:color "white" :fontSize 20}}
                        "Settings"]]
-     ;; USER INFO ---------------
+    ;; USER INFO ---------------
       [:> react/View {:height 125 :paddingBottom 20 :paddingTop 20}
         [:> react/Text {:style {:color "slategray" :fontSize 15
                                 :paddingLeft 20 :justifyContent "flex-end"}}
@@ -41,20 +42,22 @@
             [:> react/Text "USERNAME"]
             [:> react/Text {:note true :style {:color "gray"}}
                            "email"]]]]
-      ;; USER CITY ---------------
+    ;; USER CITY ---------------
       [:> react/View {:height 125}
-       [:> react/Text {:style {:color "slategray" :fontSize 15 :paddingLeft 20
-                               :justifyContent "flex-end"}}
-                      (str/upper-case "current city")]
-       [:> react/TouchableOpacity {:onPress #(navigate "select-city"
-                                                       {:user/uid  id
-                                                        :city/name (:city/name (:user/city city))})
-                                   :style {:height 45}}
-         [symbols/PointOfInterest
-           [:> expo/Ionicons {:name "md-map" :size 26}]
-           [:> react/Text ""]
-           [:> react/Text (:city/name (:user/city city))]
-           [:> react/Text {:style {:color "gray"}}
-                          (str (:city/region (:user/city city)) ", "
-                               (:city/country (:user/city city)))]
-           [:> expo/Ionicons {:name "ios-checkmark" :size 26}]]]]]))
+        [:> react/Text {:style {:color "slategray" :fontSize 15 :paddingLeft 20
+                                :justifyContent "flex-end"}}
+                       (str/upper-case "current city")]
+        [:> react/TouchableOpacity {:onPress #(navigate "select-city"
+                                                        {:user/uid  id
+                                                         :city/name (:city/name (:user/city city))})
+                                    :style {:height 45}}
+          [symbols/PointOfInterest
+            [:> expo/Ionicons {:name "md-map" :size 26}]
+            [:> react/Text ""]
+            [:> react/Text (:city/name (:user/city city))]
+            [:> react/Text {:style {:color "gray"}}
+                           (str (:city/region (:user/city city)) ", "
+                                (:city/country (:user/city city)))]
+            [:> expo/Ionicons {:name "ios-checkmark" :size 26}]]]]]))
+
+;hive.rework.state/conn
